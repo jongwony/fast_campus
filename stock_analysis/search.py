7f4218c4bbfcd3dd862f4d825b2e2ada032c1eca
jongwony <lastone9182@gmail.com>
2024-06-07 12:24:10 +0900

    change txt to csv

--- stock_analysis/search.py
+++ stock_analysis/search.py
@@ -5,12 +5,14 @@ client = meilisearch.Client('http://localhost:7700', 'aSampleMasterKey')
 
 
 def bootstrap_stocks():
-    # https://www.nasdaqtrader.com/Trader.aspx?id=symbollookup
-    stocks = pd.read_csv('nasdaqlisted.txt', sep='|')
-    stocks.reset_index(names='id', inplace=True)
-    stocks.replace(pd.NA, None, inplace=True)
-    stocks = stocks[['id', 'Symbol', 'Security Name']].to_dict(orient='records')
-    client.index('stocks').add_documents(stocks)
+    """
+    https://www.nasdaq.com/market-activity/stocks/screener
+    """
+    # json.dumps(body)[1919-30:1919+30]
+    stocks = pd.read_csv('nasdaq_screener.csv', na_filter=False)
+    # df[df['Symbol'].str.contains(r'[^\w]', regex=True)]
+    stocks['id'] = stocks['Symbol'].str.strip().replace(r'[/^]', '_', regex=True)
+    client.index('stocks').add_documents(stocks.to_dict(orient='records'), primary_key='id')
 
 
 def stock_search(query):
