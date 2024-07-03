3cafca1795a5b60a4ed9fe6acdaecb49605d2b68
jongwony <lastone9182@gmail.com>
2024-07-03 23:12:01 +0900

    API로 이슈 생성하기

--- jira_bolt/issue_tracking.py
+++ jira_bolt/issue_tracking.py
@@ -5,15 +5,54 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-jira = Jira(
-    url='https://productivitysolutionsinc.atlassian.net',
-    username='fc.jongwony@gmail.com',
-    password=os.getenv('ATLASSIAN_API_KEY'),
-    cloud=True,
-)
 
-jira.projects()
-jira.get_issue_types()
-# jira.issue_create
-jira.issue_fields('KAN-1')
-# jira.create_issue()
+class CustomJira(Jira):
+    """
+    self.get_all_custom_fields() 로 custom field id 를 찾아서 사용할 수 있음
+    """
+    fields = None
+
+    def __init__(self):
+        super().__init__(
+            url='https://productivitysolutionsinc.atlassian.net',
+            username='fc.jongwony@gmail.com',
+            password=os.getenv('ATLASSIAN_API_KEY'),
+            cloud=True,
+        )
+
+    def create_issue(self):
+        """
+        Python 3.12 부터는 override 라는 데코레이터가 있다
+        https://docs.python.org/3/whatsnew/3.12.html#pep-698-override-decorator-for-static-typing
+        """
+        return super().create_issue(self.fields)
+
+    def set_fields(self, fields: dict):
+        self.fields = fields
+
+    def task_fields(self, summary: str, description: str):
+        return {
+            'project': {'key': 'KAN'},
+            'issuetype': {'name': '작업'},
+            'summary': summary,
+            'description': description,
+        }
+
+    def bug_fields(self, summary: str, description: str, stage: str):
+        return {
+            'project': {'key': 'KAN'},
+            'issuetype': {'name': '버그'},
+            'summary': summary,
+            'description': description,
+            'customfield_10033': {'value': stage},
+        }
+
+
+if __name__ == '__main__':
+    jira = CustomJira()
+    # jira.get_all_custom_fields()
+    # fields = jira.task_fields('테스트', '테스트')
+    fields = jira.bug_fields('테스트', '테스트', 'dev')
+    jira.set_fields(fields)
+    response = jira.create_issue()
+    print(response)
